5e1df82a85f872d22ce32cb0310804835f03bc51
pschild <philippe.schild@gmail.com>
2018-02-28 19:58:25 +0100

    tabs -> spaces

--- switch.js
+++ switch.js
@@ -2,39 +2,41 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const rpi433 = require('rpi-433');
 
 /*
 rfSniffer = rpi433.sniffer({
-	pin: 2,
-	debounceDelay: 500
+    pin: 2,
+    debounceDelay: 500
 });
 rfSniffer.on('data', (data) => {
-	console.log('received', data.code, data.pulseLength);
+    console.log('received', data.code, data.pulseLength);
 });
 */
 
 rfEmitter = rpi433.emitter({
-	pin: 0,
-	pulseLength: 350
+    pin: 0,
+    pulseLength: 350
 });
 
 app.get('/switch/:code', function (req, res) {
-	let code = +req.params.code; // 4433 = on, 4436 = off for Switch "B"
-	console.log(code);
-	rfEmitter.sendCode(code)
-		.then((stdout) => {
-			res.json({
-    				'success': true,
-				'result': stdout
-		  	});
-		}, (error) => {
-			console.log('Error', error);
-		});
+    let code = +req.params.code; // 4433 = on, 4436 = off for Switch "B"
+    console.log(code);
+    rfEmitter.sendCode(code)
+        .then((stdout) => {
+            res.json({
+                'success': true,
+                'result': stdout
+            });
+        }, (error) => {
+            console.log('Error', error);
+        });
 });
 
 app.listen(8080, () => {
-	console.log('Server is running on Port 8080');
+    console.log('Server is running on Port 8080');
 });
