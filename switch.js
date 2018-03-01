be7738c06c5537d2090982b3b660c02532e0d1aa
pschild <philippe.schild@gmail.com>
2018-03-01 11:18:30 +0100

    code format

--- switch.js
+++ switch.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-const privateKey  = fs.readFileSync('../../dehydrated/certs/pschild.duckdns.org/privkey.pem', 'utf8');
+const privateKey = fs.readFileSync('../../dehydrated/certs/pschild.duckdns.org/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('../../dehydrated/certs/pschild.duckdns.org/fullchain.pem', 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
@@ -51,30 +51,30 @@ app.get('/switch/:code', function (req, res) {
 });
 
 app.post('/alexa', function (req, res) {
-    let { switchNameSlot, switchActionSlot } = req.body.payload;
+    let {switchNameSlot, switchActionSlot} = req.body.payload;
 
     let spokenNameValue = switchNameSlot.value;
     let mappedNameFromSynonym = switchNameSlot.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-    
+
     let spokenActionValue = switchActionSlot.value;
     let mappedActionFromSynonym = switchActionSlot.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-    
+
     console.log(mappedNameFromSynonym, mappedActionFromSynonym);
-    
+
     let code;
     switch (mappedNameFromSynonym) {
-		case 'Fernseher':
-		    code = mappedActionFromSynonym === 'ein' ? 1361 : 1364;
-		    break;
-		case 'Test':
-		    code = mappedActionFromSynonym === 'ein' ? 4433 : 4436;
-		    break;
-		case 'Anlage':
-		    code = mappedActionFromSynonym === 'ein' ? 5201 : 5204;
-		    break;
-	}
-	
-	rfEmitter.sendCode(code)
+        case 'Fernseher':
+            code = mappedActionFromSynonym === 'ein' ? 1361 : 1364;
+            break;
+        case 'Test':
+            code = mappedActionFromSynonym === 'ein' ? 4433 : 4436;
+            break;
+        case 'Anlage':
+            code = mappedActionFromSynonym === 'ein' ? 5201 : 5204;
+            break;
+    }
+
+    rfEmitter.sendCode(code)
         .then((stdout) => {
             res.json({
                 'success': true,
